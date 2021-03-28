c399cc90885f688f4889adf9f302293d7955b7e4
hakerg <grzegorzabedzki@gmail.com>
2021-03-29 00:48:07 +0200

    Add files via upload

--- zad1.py
+++ zad1.py
@@ -16,67 +16,78 @@ def create_distance_matrix(nodes):
     return distance_matrix
 
 def nearest_neighbor(distance_matrix):
-    order = [0]
-    to_pick = list(range(1, len(distance_matrix)))
+    count = math.ceil(len(distance_matrix) / 2)
+    order = [random.randrange(0, len(distance_matrix))]
+    to_pick = [i for i in range(len(distance_matrix)) if i not in order]
+    to_pick.sort(key=lambda index: distance_matrix[order[0]][index])
+    to_pick = to_pick[:count - 1]
+
     while len(to_pick) > 0:
         distance_list = distance_matrix[order[-1]]
         shortest = min(to_pick, key=lambda index: distance_list[index])
         order.append(shortest)
         to_pick.remove(shortest)
+    
+    order.append(order[0])
     return order
 
 def greedy_expand(distance_matrix):
-    min_i = 0
-    min_j = 1
-    for i in range(len(distance_matrix)):
-        for j in range(len(distance_matrix)):
-            if i != j and distance_matrix[i][j] < distance_matrix[min_i][min_j]:
-                min_i = i
-                min_j = j
-    order = [min_i, min_j]
-    to_pick = list(range(len(distance_matrix)))
-    to_pick.remove(min_i)
-    to_pick.remove(min_j)
+    count = math.ceil(len(distance_matrix) / 2)
+    order = [random.randrange(0, len(distance_matrix))]
+    to_pick = [i for i in range(len(distance_matrix)) if i not in order]
+    to_pick.sort(key=lambda index: distance_matrix[order[0]][index])
+    to_pick = to_pick[:count - 1]
+    order.append(order[0])
+
     while len(to_pick) > 0:
-        distance_list_1 = distance_matrix[order[-1]]
-        distance_list_2 = distance_matrix[order[-2]]
-        new_index = min(to_pick, key=lambda index: distance_list_1[index] + distance_list_2[index])
-        order.append(new_index)
-        to_pick.remove(new_index)
+        min_cost = math.inf
+        min_index = None
+        min_place = None
+        for index in to_pick:
+            for place in range(1, len(order)):
+                index_before = order[place - 1]
+                index_after = order[place]
+                cost = distance_matrix[index_before][index] + distance_matrix[index][index_after] - distance_matrix[index_before][index_after]
+                if cost < min_cost:
+                    min_cost = cost
+                    min_index = index
+                    min_place = place
+        
+        order.insert(min_place, min_index)
+        to_pick.remove(min_index)
     return order
 
 def regret_heuristics(distance_matrix):
-    min_i = 0
-    min_j = 1
-    for i in range(len(distance_matrix)):
-        for j in range(len(distance_matrix)):
-            if i != j and distance_matrix[i][j] < distance_matrix[min_i][min_j]:
-                min_i = i
-                min_j = j
-    order = [min_i, min_j]
-    to_pick = list(range(len(distance_matrix)))
-    to_pick.remove(min_i)
-    to_pick.remove(min_j)
-    while len(to_pick) > 1:
-        profit_table = []
-        best_profits = [-math.inf, -math.inf]
-        for index in to_pick:
-            profit_list = [-distance_matrix[order[-1]][index] - distance_matrix[order[-2]][index]]
-            profit_list.append(max(-distance_matrix[i][index] - distance_matrix[i][order[-1]] for i in to_pick if i != index))
-            profit_table.append(profit_list)
-            for i in range(2):
-                if profit_list[i] > best_profits[i]:
-                    best_profits[i] = profit_list[i]
-        min_regret = math.inf
-        new_index = None
-        for i in range(len(to_pick)):
-            regret = max(best_profits[0] - profit_table[i][0], best_profits[1] - profit_table[i][1])
-            if regret < min_regret:
-                min_regret = regret
-                new_index = to_pick[i]
-        order.append(new_index)
-        to_pick.remove(new_index)
+    count = math.ceil(len(distance_matrix) / 2)
+    order = [random.randrange(0, len(distance_matrix))]
+    to_pick = [i for i in range(len(distance_matrix)) if i not in order]
+    to_pick.sort(key=lambda index: distance_matrix[order[0]][index])
+    to_pick = to_pick[:count - 1]
+    
     order.append(to_pick[0])
+    to_pick.remove(to_pick[0])
+    order.append(order[0])
+
+    while len(to_pick) > 0:
+        max_regret = -math.inf
+        max_index = None
+        max_place = None
+        for index in to_pick:
+            places = []
+            for place in range(1, len(order)):
+                index_before = order[place - 1]
+                index_after = order[place]
+                cost = distance_matrix[index_before][index] + distance_matrix[index][index_after] - distance_matrix[index_before][index_after]
+                places.append((place, cost))
+            places.sort(key=lambda item: item[1])
+            regret = places[1][1] - places[0][1]
+            if regret > max_regret:
+                max_regret = regret
+                max_index = index
+                max_place = places[0][0]
+        
+        order.insert(max_place, max_index)
+        to_pick.remove(max_index)
     return order
 
 filenames = ['kroA100.tsp', 'kroB100.tsp']
@@ -89,30 +100,27 @@ names = {
 for filename in filenames:
     problem = tsplib95.load(os.path.join(sys.path[0], filename))
     nodes = list(problem.node_coords.values())
+    distance_matrix = create_distance_matrix(nodes)
 
     for method in methods:
         lengths = []
         best_length = math.inf
-        best_nodes = None
         best_order = None
         for _ in range(50):
-            half_nodes = random.sample(nodes, math.ceil(len(nodes) / 2))
-            distance_matrix = create_distance_matrix(half_nodes)
             order = method(distance_matrix)
-            order.append(order[0])
             length = sum(distance_matrix[order[i]][order[i + 1]] for i in range(len(order) - 1))
             lengths.append(length)
             if length < best_length:
                 best_length = length
-                best_nodes = half_nodes
                 best_order = order
 
-        x = [node[0] for node in best_nodes]
-        y = [node[1] for node in best_nodes]
-        line_x = [best_nodes[i][0] for i in best_order]
-        line_y = [best_nodes[i][1] for i in best_order]
+        x = [node[0] for node in nodes]
+        y = [node[1] for node in nodes]
+        line_x = [nodes[i][0] for i in best_order]
+        line_y = [nodes[i][1] for i in best_order]
 
-        plt.title('{0}, {1}\navg: {2}, min: {3}, max: {4}'.format(filename, names[method], np.average(lengths), min(lengths), max(lengths)))
+        print("{0}, {1}: avg: {2}, min: {3}, max: {4}".format(filename, names[method], np.average(lengths), min(lengths), max(lengths)))
+        plt.title("{0}, {1}".format(filename, names[method]))
         plt.scatter(x, y)
         plt.plot(line_x, line_y)
         plt.show()
